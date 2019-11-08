8e749ef0ac393b0839a7b827d1485eb45ec01058
Oliver <ocben1@gmail.com>
2019-11-09 10:59:23 +1100

    using graphql tools

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import schema from './schema';
-import resolvers from './resolvers'
+import { schema } from './schema';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
-
-const root = resolvers;
 
 app.use('/graphql',  graphqlHTTP({
     schema: schema,
